f81be56834665a9851e462f16b2d26e756b91915
Karthik Ram <karthik.ram@gmail.com>
2019-05-24 14:32:20 -0700

    Updated dockerfile write implementation

--- R/write_dockerfile.R
+++ R/write_dockerfile.R
@@ -10,29 +10,35 @@
 #' @export
 #'
 write_dockerfile <-
-  function(base = "rocker/binder:latest", path = ".")
+  function(base = "rocker/binder:latest", path = ".", method = "desc")
   {
     user <- gh::gh_tree_remote(path)$username
     repo <- gh::gh_tree_remote(path)$repo
-    z <- containerit::dockerfile(
+    z <- suppressWarnings(containerit::dockerfile(
       from = NULL,
       image = base,
       add_self = FALSE,
       container_workdir = NULL,
       silent = TRUE,
-      cmd = containerit::Cmd("R")
+      cmd = containerit::Cmd("R"))
     )
    
-    containerit::write(z, file = "Dockerfile")
+    suppressWarnings(containerit::write(z, file = "Dockerfile"))
     line1 = "USER root"
     line2 = "COPY . ${HOME}"
     line3 = "RUN chown -R ${NB_USER} ${HOME}"
     line4 = "## Become normal user again"
     line5 = "USER ${NB_USER}"
+    if(method == "desc") {
     line6 = glue(
       "RUN wget https://github.com/{user}/{repo}/raw/master/DESCRIPTION && \
 R -e \"devtools::install_deps()\""
     )
+  }
+  if(method == "install") {
+    line6 <- "R -e \"source('install.R')\""
+    write_install(path)
+  }
     df <- readLines("Dockerfile")
     df <- df[1:2]
     writeLines(df, con = "Dockerfile")
