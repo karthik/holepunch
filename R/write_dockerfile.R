e8fb7a86cd7b86b1cd0eaa180bf42590ad988680
Karthik Ram <karthik.ram@gmail.com>
2019-06-13 14:30:45 -0700

    Working on fix to Dockerfile #8

--- R/write_dockerfile.R
+++ R/write_dockerfile.R
@@ -1,53 +1,57 @@
 
 
 
+
 #' Write a minimal Dockerfile for a binder
 #'
 #' @param base Rocker base
+#' @param maintainer Maintainer of the Dockerfile
+#' @param r_version  Version of R you wish to use. Will default to latest for now.
 #' @param path path to binder repo
-#' @param method If left blank, it will install using the DESCRIPTION file. Otherwise it will use the install.R file.
+#'
 #' @importFrom glue glue
 #' @export
 #'
 write_dockerfile <-
-  function(base = "rocker/binder:latest", path = ".", method = "desc")
+  function(base = NULL,
+           path = ".",
+           maintainer = "karthik",
+           r_version = "latest")
   {
     user <- gh::gh_tree_remote(path)$username
     repo <- gh::gh_tree_remote(path)$repo
-    z <- suppressWarnings(containerit::dockerfile(
-      from = NULL,
-      image = base,
-      add_self = FALSE,
-      container_workdir = NULL,
-      silent = TRUE,
-      cmd = containerit::Cmd("R"))
-    )
-    suppressWarnings(containerit::write(z, file = "Dockerfile"))
-    line1 = "USER root"
-    line2 = "COPY . ${HOME}"
-    line3 = "RUN chown -R ${NB_USER} ${HOME}"
-    line4 = "## Become normal user again"
-    line5 = "USER ${NB_USER}"
-    if(method == "desc") {
-    line6 = glue(
-      "RUN wget https://github.com/{user}/{repo}/raw/master/DESCRIPTION && R -e \"devtools::install_deps()\""
-    )
-  }
-  if(method == "install") {
-    line6 <- "R -e \"source('install.R')\""
-    write_install(path)
-  }
+    
+    # TODO
+    # -------------
+    # IF base is null and r_version = latest, then make FROM
+    # rocker/binder:latest If base is set, use that base and latest date a file
+    # was touched If base is NULL and r_version = a date, use R version for that
+    # date (from look up table) and same date for packages
+    
+    # -------------
+    
+    R_VERSION = r_version # TODO: This needs to be a function that returns R-version for a given date
+    DATE = last_modification_date(".")
+    MAINTAINER = "maintainer"
+    glue::glue(
+      "
+FROM rocker/binder:[R_VERSION]
+LABEL maintainer='[MAINTAINER]'
+USER root
+COPY . ${HOME}
+RUN chown -R ${NB_USER} ${HOME}
+USER ${{NB_USER}}
+RUN wget https://github.com/[user]/[repo]/raw/master/DESCRIPTION && R -e 'options(repos = list(CRAN = 'http://mran.revolutionanalytics.com/snapshot/[DATE]/'); 'devtools::install_deps()'
+",
+      .open = "[",
+      .close = "]"
+    ) -> docker_contents
     
     
-    df <- readLines("Dockerfile")
-    df <- df[1:2]
-    writeLines(df, con = "Dockerfile")
-    write(line1, file = "Dockerfile", append = TRUE)
-    write(line2, file = "Dockerfile", append = TRUE)
-    write(line3, file = "Dockerfile", append = TRUE)
-    write(line4, file = "Dockerfile", append = TRUE)
-    write(line5, file = "Dockerfile", append = TRUE)
-    write(line6, file = "Dockerfile", append = TRUE)
+    fileConn <- file("Dockerfile")
+    writeLines(docker_contents, fileConn)
+    close(fileConn)
+    
     fs::dir_create(".binder")
     fs::file_move("Dockerfile", ".binder/Dockerfile")
   }
