9e382e33c4b42abead42842d866f9a47838018c6
Karthik Ram <karthik.ram@gmail.com>
2019-06-13 10:10:42 -0700

    Added last modification date

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+
+
+#' Last Modified late
+#'
+#' Returns the latest date for when a file was touched in a project
+#' @param path 
+#'
+#' @return
+#' @internal
+last_modification_date <- function(path = ".") {
+  dir_list <- fs::dir_info(path)
+  sorted_dir_list <- dir_list[order(dir_list$modification_time, decreasing  = TRUE), ]
+  last_mod <- sorted_dir_list[1, ]$modification_time
+  as.Date(last_mod)
+}
