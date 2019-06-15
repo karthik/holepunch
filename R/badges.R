107bff1fa8883a6242504576e26160e53670919e
Karthik Ram <karthik.ram@gmail.com>
2019-06-15 12:07:44 -0700

    Fixed glue issue

--- R/badges.R
+++ R/badges.R
@@ -17,14 +17,9 @@ generate_badge <-  function(path  = ".", hub = "mybinder.org", urlpath = "rstudi
     user <- gh_tree_remote(path)$username
     repo <- gh_tree_remote(path)$repo
     url <-
-      glue("https://{hub}/v2/gh/",
-           {user},
-           "/",
-           {repo},
-           "/master?urlpath={urlpath}")
-      # TODO: mybinder is hard coded. Must fix.
+      glue("https://{hub}/v2/gh/{user}/{repo}/master?urlpath={urlpath}")
     img <- glue("http://{hub}/badge.svg")
-    use_badge("Launch Rstudio binder", url, img)
+    use_badge("Launch Rstudio Binder", url, img)
   }
   
   invisible(TRUE)
