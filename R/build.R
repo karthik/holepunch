44482312bbdff4d4192a82db83ab292cc229feac
Karthik Ram <karthik.ram@gmail.com>
2019-06-19 19:13:33 -0700

    Build now happens in the background!

--- R/build.R
+++ R/build.R
@@ -6,60 +6,52 @@
 #' binder is infrequently used), the build can take a long time.
 #' @param path path to local git controlled folder
 #' @template hub
+#' @param urlpath RStudio
 #'
 #' @importFrom httr GET content
 #' @importFrom cliapp cli_alert_warning
 #' @export
-build_binder <- function(path = ".", hub = "mybinder.org") {
+binder_builder <- function(path = ".", hub = "mybinder.org", urlpath = "rstudio") {
  
-  if(!is_clean(path)) {
-    stop("Please commmit and push files to GitHub before building binder. Otherwise Binder cannot see these new files/changes", call. = FALSE)
-  }
-  
   path <- sanitize_path(path)
-  cliapp::cli_alert_warning("This may take a while but you can kill this process and the build will still continue")
+  # cliapp::cli_alert_warning("This may take a while but you can kill this process and the build will still continue")
   user <- gh_tree_remote(path)$username
   repo <- gh_tree_remote(path)$repo
   # binder_runtime <- paste0("https://mybinder.org/build/gh/", user, "/", repo, "/master")
   binder_runtime <-
     glue::glue("https://{hub}/build/gh/{user}/{repo}/master")
-  cliapp::cli_alert_info(glue::glue("Building binder at {binder_runtime}"))
   res <- httr::GET(binder_runtime)
-  parse_streamer(
-    url = binder_runtime
-  )
-  return(binder_runtime)
+  # parse_streamer(
+  #   url = binder_runtime
+  # )
+  url <-
+    glue("https://{hub}/v2/gh/{user}/{repo}/master?urlpath={urlpath}")
+  return(url)
 }
 
-#' @noRd
-#' @importFrom jsonlite fromJSON
-#' @importFrom curl curl
-#' @keywords internal
-parse_streamer <-
-  function(url,
-           cb = print,
-           hub = "mybinder.org",
-           urlpath = "rstudio") {
-    # Many thanks to Jeroem Ooms for this parser!
-    con <- curl::curl(url = url)
-    open(con)
-    on.exit(close(con))
-    repeat {
-      txt <- readLines(con, 1)
-      if (!length(txt)) {
-        print("All done!")
-        cb(NULL)
-        break
-        
-      }
-      if (grepl("^data:", txt)) {
-        json <- sub("^data:", "", txt)
-        data <- jsonlite::fromJSON(json)
-        cb(data)
-      }
-    }
-    badge_url <- gsub("https://mybinder.org/build/gh/", "", url)
-    full_badge_url <-
-      glue::glue("https://{hub}/v2/gh/{badge_url}/?urlpath={urlpath}")
-    utils::browseURL(full_badge_url)
+
+#' Builds binder
+#'
+#' This function kicks off the image build on binder. Although it is not
+#' entirely necessary to run this step, doing so will ensure that there is a
+#' built image on binder ready to launch. Otherwise the first time (or if your
+#' binder is infrequently used), the build can take a long time.
+#' @param path path to local git controlled folder
+#' @template hub
+#' @param urlpath RStudio
+#' @export
+build_binder <- function(path = ".", hub = "mybinder.org", urlpath = "rstudio") {
+  
+  if(!is_clean(path)) {
+    stop("Please commmit and push files to GitHub before building binder. Otherwise Binder cannot see these new files/changes", call. = FALSE)
   }
+  
+
+  cliapp::cli_alert_info(glue::glue("Your Binder is being built in the background. Once built, your browser will automatically launch. You can also click the binder badge at a later time too"))
+  `%...>%` <- promises::`%...>%`
+  multisession <- 'future' %:::% 'multisession'
+  future::plan(multisession, workers = 2)
+  future::future({
+    binder_builder(path, hub, urlpath)
+  }) %...>% utils::browseURL
+}
