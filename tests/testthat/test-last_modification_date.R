523e85aa1bc5951fae35fcc9ca3d04582dae91b0
Karthik Ram <karthik.ram@gmail.com>
2019-06-20 11:13:10 -0700

    fixed last mod date test

--- tests/testthat/test-last_modification_date.R
+++ tests/testthat/test-last_modification_date.R
@@ -1,8 +1,7 @@
 test_that("last modification date", {
-  
   fs::file_create("foo")
-  fs::file_touch("foo", "2018-01-01")
-  touch_date <- holepunch:::last_modification_date("foo")
-  expect_identical("2018-01-01", touch_date)
-  
+  today = lubridate::ymd(lubridate::today())
+  fs::file_touch("foo", today)
+  touch_date <- holepunch:::last_modification_date(".")
+  expect_identical(today, touch_date)
 })
