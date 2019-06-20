a596697bcf39a40060cfde2bb940326de87d83d7
Karthik Ram <karthik.ram@gmail.com>
2019-06-20 11:21:31 -0700

    Fixed stray file

--- tests/testthat/test-last_modification_date.R
+++ tests/testthat/test-last_modification_date.R
@@ -4,4 +4,5 @@ test_that("last modification date", {
   fs::file_touch("foo", today)
   touch_date <- holepunch:::last_modification_date(".")
   expect_identical(today, touch_date)
+  unlink("foo")
 })
