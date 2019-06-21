50d6a6fb3a76fec277c96e3a81d7c209dbefba35
Karthik Ram <karthik.ram@gmail.com>
2019-06-21 14:50:59 -0700

    Testing helper functions

--- tests/testthat/test-write_compendium_description.R
+++ tests/testthat/test-write_compendium_description.R
@@ -1,18 +1,56 @@
 test_that("Write Compendium Description", {
-  library(glue)
-  x <- tempdir(check = TRUE)
-  message(glue("Path for tempdir is {x}"))
-  fs::dir_create(glue("{x}/.binder"))
+  # This doesn't work even if I put the function in 
+  # helper-write_compendium_description.R 
+  # test_path <- set_up_test_repo()
+  # WHY???
+  # ----------------------------------------------
+  
+  set_up_test_repo <- function() {
+    test_path <- paste0(tempdir(), "/testcompendium")
+    dir.create(test_path, showWarnings = FALSE)
+    git2r::init(test_path)
+    # This is a horrifying way to write a config manually, but
+    # . ¯\_(ツ)_/¯
+    # I don't know a better way for now.
+    config <- glue::glue(
+      "
+[core]
+  bare = false
+  repositoryformatversion = 0
+  filemode = true
+  ignorecase = true
+  precomposeunicode = true
+  logallrefupdates = true
+[remote \"origin\"]
+  url = git@github.com:user/repo.git
+  fetch = +refs/heads/*:refs/remotes/origin/*
+[branch \"master\"]
+  remote = origin
+  merge = refs/heads/master
+"
+    )
+    fileConn <- file(glue::glue("{test_path}/.git/config"))
+    writeLines(config, fileConn)
+    close(fileConn)
+    cat(
+      "```{r}\nlibrary(dplyr)\nrequire(ggplot2)\nglue::glue_collapse(glue::glue('{1:10}'))\n```\n",
+      file = paste0(test_path, "/test.Rmd")
+    )
+    return(test_path)
+  }
+  
+  test_path <- set_up_test_repo()
+  fs::dir_create(glue::glue("{test_path}/.binder"))
   z <- glue::glue("
             library(curl)
             library(fs)
             library(here)
             fs::dir_exists('.')
             ")
-  fileConn <- file(glue("{x}/z.R"))
+  fileConn <- file(glue::glue("{test_path}/z.R"))
   writeLines(z, fileConn)
   close(fileConn)
-  write_compendium_description(path = x)
-  expect_true(fs::file_exists(glue("{x}/DESCRIPTION")))
-  fs::dir_delete(x)
+  write_compendium_description(path = test_path)
+  expect_true(fs::file_exists(glue::glue("{test_path}/DESCRIPTION")))
+  unlink(test_path)
 })
--- tests/testthat/test-write_dockerfile.R
+++ tests/testthat/test-write_dockerfile.R
@@ -1,41 +1,40 @@
 test_that("Writing Dockerfile works", {
-  test_path <- paste0(tempdir(), "/testcompendium")
-  dir.create(test_path, showWarnings = FALSE)
-  old_path = getwd()
-  # Note: suppressing warnings here because if I don't I see this:
-  # warning: `recursive` is deprecated, please use `recurse` instead
-  suppressWarnings(usethis::create_project(path = test_path, open = FALSE))
-  setwd(test_path)
+  temp <- tempdir()
+  rand_string <-
+    paste(sample(c(0:9, letters, LETTERS), 12, replace = TRUE), collapse = "")
+  test_path <- glue::glue("{temp}/{rand_string}/testcompendium")
+  print(glue::glue("test path is {test_path}"))
+  fs::dir_create(test_path)
   git2r::init(test_path)
-  
   # This is a horrifying way to write a config manually, but
   # . ¯\_(ツ)_/¯
   # I don't know a better way for now.
   config <- glue::glue(
     "
 [core]
-	bare = false
-	repositoryformatversion = 0
-	filemode = true
-	ignorecase = true
-	precomposeunicode = true
-	logallrefupdates = true
+  bare = false
+  repositoryformatversion = 0
+  filemode = true
+  ignorecase = true
+  precomposeunicode = true
+  logallrefupdates = true
 [remote \"origin\"]
-	url = git@github.com:user/repo.git
-	fetch = +refs/heads/*:refs/remotes/origin/*
+  url = git@github.com:user/repo.git
+  fetch = +refs/heads/*:refs/remotes/origin/*
 [branch \"master\"]
-	remote = origin
-	merge = refs/heads/master
+  remote = origin
+  merge = refs/heads/master
 "
   )
   fileConn <- file(glue::glue("{test_path}/.git/config"))
   writeLines(config, fileConn)
-  
+  close(fileConn)
   cat(
     "```{r}\nlibrary(dplyr)\nrequire(ggplot2)\nglue::glue_collapse(glue::glue('{1:10}'))\n```\n",
     file = paste0(test_path, "/test.Rmd")
   )
   
+  print(test_path)
   library(holepunch)
   write_compendium_description(path = test_path)
   write_dockerfile(path = test_path, maintainer = "Wes Anderson")
@@ -48,6 +47,5 @@ test_that("Writing Dockerfile works", {
   expect_identical(reading_back_dockerfile[4], "COPY . ${HOME}")
   expect_identical(reading_back_dockerfile[5], "RUN chown -R ${NB_USER} ${HOME}")
   
-  unlink(test_path)
-  setwd(old_path)
+ #  unlink(test_path)
 })
