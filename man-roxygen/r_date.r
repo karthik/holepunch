f2502290f87a606be273ff7a74cd0e8b9eea3ce3
Karthik Ram <karthik.ram@gmail.com>
2019-07-01 14:07:57 -0700

    fixes #27

--- man-roxygen/r_date.r
+++ man-roxygen/r_date.r
@@ -1,4 +1,4 @@
 #' @param r_date Date you need R to lock down to. By default it picks the most
 #'   recent date a file was touched in this project but you can override this by
-#'   specifying this explicitly. Date must be in ISO 8601 format.
-
+#'   specifying this explicitly. Date must be in ISO 8601 format, e.g.
+#'   2019-06-27
