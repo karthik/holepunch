1c2e05d4dfcfa0ff94a6a7404c64e98b8cae562d
Anna Krystalli <annakrystalli@googlemail.com>
2023-02-10 12:23:06 +0200

    change copy command. Stops Rmd to pdf rights errors

--- tests/testthat/test-write_dockerfile.R
+++ tests/testthat/test-write_dockerfile.R
@@ -8,9 +8,8 @@ test_that("Writing Dockerfile works", {
     readLines(glue::glue("{temp_path}/.binder/Dockerfile"))
 
   expect_identical(reading_back_dockerfile[2], "LABEL maintainer='Wes Anderson'")
-  expect_identical(reading_back_dockerfile[3], "USER root")
-  expect_identical(reading_back_dockerfile[4], "COPY . ${HOME}")
-  expect_identical(reading_back_dockerfile[5], "RUN chown -R ${NB_USER} ${HOME}")
+  expect_identical(reading_back_dockerfile[3], "COPY --chown=${NB_USER} . ${HOME}")
+  expect_identical(reading_back_dockerfile[4], "USER ${NB_USER}")
 
   unlink(temp_path)
   setwd(old_path)
